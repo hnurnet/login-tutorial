3b42025668b7e82a5d61ebfe9af1c26bf1fb30b0
Hassan-nur <hnurm@hotmail.com>
2022-12-21 19:14:17 -0500

    Added new files

---
+++ frontend/src/app/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup = new FormGroup({
+    email:new FormControl(null, [Validators.email, Validators.required]),
+    password:new FormControl(null, Validators.required)
+
+  })
+  constructor(private _router:Router){}
+
+  ngOnInit(){}
+
+  movetoRegister(){
+    this._router.navigate(['/register']);
+
+  }
+
+  login(){
+    if(!this.loginForm.valid){
+      console.log('Invalid'); return;
+
+    }
+    console.log(JSON.stringify(this.loginForm.value));
+
+  }
+
+}
